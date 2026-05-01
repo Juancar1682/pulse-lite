d9c776c7ae1a93a3fc83a71a7e8747507fc78895
Juancar1682 <96840752+Juancar1682@users.noreply.github.com>
2026-05-01 10:12:55 -0400

    chore: add post route for creatitng new patients and add validation so the data is in range of acceptable

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,47 @@ interface Vitals {
   bloodOxygen: number;
 }
 
+app.use(express.json());
+
+app.post("/patient", async (req, res) => {
+  const { name, age, consciousness, bp, heartRate, bloodOxygen } = req.body;
+  if (age < 0 || age > 120) {
+    return res.status(400).json("Invalid age number");
+  }
+  if (bp < 60 || bp > 160) {
+    return res.status(400).json("Invalid bp number");
+  }
+  if (heartRate < 60 || heartRate > 180) {
+    return res.status(400).json("Invalid heartRate number");
+  }
+  if (bloodOxygen < 75 || bloodOxygen > 100) {
+    return res.status(400).json("Invalid bloodOxygen number");
+  }
+  res.json(
+    (
+      await pool.query(
+        `INSERT INTO "vitals" (
+        "name", 
+        "age", 
+        "consciousness", 
+        "bp", 
+        "heartRate", 
+        "bloodOxygen" )
+       VALUES(
+       $1,
+       $2,
+       $3,
+       $4,
+       $5,
+       $6
+    )
+    RETURNING *`,
+        [name, age, consciousness, bp, heartRate, bloodOxygen],
+      )
+    ).rows[0],
+  );
+});
+
 app.get("/vitals", async (req, res) =>
   res.json((await pool.query('SELECT * from "vitals"')).rows),
 );
