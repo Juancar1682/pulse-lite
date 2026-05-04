059d30890a479be4c149790fdf3bcf07376f6fc4
Juancar1682 <96840752+Juancar1682@users.noreply.github.com>
2026-05-04 07:43:54 -0400

    chore: add a new patient form and added funtionality so it renders without the page refreshing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,25 @@ export default function App() {
     bloodOxygen: number;
   }
 
+  interface NewPatient {
+    name: string;
+    age: number;
+    bp: number;
+    consciousness: boolean;
+    heartRate: number;
+    bloodOxygen: number;
+  }
+
   const [vitals, setVitals] = useState<Vitals[]>([]);
+
+  const [newPatient, setNewPatient] = useState<NewPatient>({
+    name: "",
+    age: 0,
+    bp: 0,
+    consciousness: false,
+    heartRate: 0,
+    bloodOxygen: 0,
+  });
 
   useEffect(() => {
     fetch("https://pulse-lite.onrender.com/vitals")
@@ -32,6 +50,72 @@ export default function App() {
           </ul>
         ))}
       </div>
+
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          const response = await fetch(
+            "https://pulse-lite.onrender.com/patient",
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(newPatient),
+            },
+          );
+          setVitals([...vitals, await response.json()]);
+        }}
+      >
+        <input
+          value={newPatient.name}
+          placeholder="name"
+          onChange={(e) =>
+            setNewPatient({ ...newPatient, name: e.target.value })
+          }
+        />
+        <input
+          value={newPatient.age}
+          onChange={(e) =>
+            setNewPatient({ ...newPatient, age: Number(e.target.value) })
+          }
+        />
+        <input
+          value={newPatient.bp}
+          onChange={(e) =>
+            setNewPatient({
+              ...newPatient,
+              bp: Number(e.target.value),
+            })
+          }
+        />
+        <input
+          type="checkbox"
+          checked={newPatient.consciousness}
+          onChange={(e) =>
+            setNewPatient({ ...newPatient, consciousness: e.target.checked })
+          }
+        />
+        <input
+          value={newPatient.heartRate}
+          onChange={(e) =>
+            setNewPatient({
+              ...newPatient,
+              heartRate: Number(e.target.value),
+            })
+          }
+        />
+        <input
+          value={newPatient.bloodOxygen}
+          onChange={(e) =>
+            setNewPatient({
+              ...newPatient,
+              bloodOxygen: Number(e.target.value),
+            })
+          }
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
