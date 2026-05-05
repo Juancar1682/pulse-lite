40d0935bdf4946500d4f6bcfc10236d487a576eb
Juancar1682 <96840752+Juancar1682@users.noreply.github.com>
2026-05-05 15:25:34 -0400

    chore: add the delete and edit button to the UI for user handling CRUD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -31,6 +31,17 @@ export default function App() {
     bloodOxygen: 0,
   });
 
+  const [edit, setEdit] = useState<number | null>(null);
+
+  const [editPatient, setEditPatient] = useState<NewPatient>({
+    name: "",
+    age: 0,
+    bp: 0,
+    consciousness: false,
+    heartRate: 0,
+    bloodOxygen: 0,
+  });
+
   useEffect(() => {
     fetch("https://pulse-lite.onrender.com/vitals")
       .then((res) => res.json())
@@ -47,6 +58,123 @@ export default function App() {
             <li>consciousness: {v.consciousness ? "yes" : "no"}</li>
             <li>heart Rate: {v.heartRate}</li>
             <li>Blood Oxygen: {v.bloodOxygen}</li>
+            <li>
+              <button
+                onClick={async () => {
+                  await fetch(
+                    `https://pulse-lite.onrender.com/patient/${v.id}`,
+                    {
+                      method: "DELETE",
+                      headers: { "Content-Type": "application/json" },
+                    },
+                  );
+                  setVitals(vitals.filter((vit) => vit.id !== v.id));
+                }}
+              >
+                X
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={async () => {
+                  setEdit(v.id);
+                  setEditPatient({
+                    name: v.name,
+                    age: v.age,
+                    bp: v.bp,
+                    consciousness: v.consciousness,
+                    heartRate: v.heartRate,
+                    bloodOxygen: v.bloodOxygen,
+                  });
+                }}
+              >
+                edit
+              </button>
+            </li>
+
+            {edit === v.id && (
+              <form
+                onSubmit={async (e) => {
+                  e.preventDefault();
+
+                  const response = await fetch(
+                    `https://pulse-lite.onrender.com/patient/${v.id}`,
+                    {
+                      method: "PUT",
+                      headers: { "Content-Type": "application/json" },
+                      body: JSON.stringify(editPatient),
+                    },
+                  );
+                  const data = await response.json();
+
+                  setVitals(
+                    vitals.map((vit) => {
+                      if (vit.id === v.id) {
+                        return data;
+                      } else return vit;
+                    }),
+                  );
+
+                  setEdit(null);
+                }}
+              >
+                <input
+                  value={editPatient.name}
+                  placeholder="name"
+                  onChange={(e) =>
+                    setEditPatient({ ...editPatient, name: e.target.value })
+                  }
+                />
+                <input
+                  value={editPatient.age}
+                  onChange={(e) =>
+                    setEditPatient({
+                      ...editPatient,
+                      age: Number(e.target.value),
+                    })
+                  }
+                />
+                <input
+                  value={editPatient.bp}
+                  onChange={(e) =>
+                    setEditPatient({
+                      ...editPatient,
+                      bp: Number(e.target.value),
+                    })
+                  }
+                />
+                <input
+                  type="checkbox"
+                  checked={editPatient.consciousness}
+                  onChange={(e) =>
+                    setEditPatient({
+                      ...editPatient,
+                      consciousness: e.target.checked,
+                    })
+                  }
+                />
+                <input
+                  value={editPatient.heartRate}
+                  onChange={(e) =>
+                    setEditPatient({
+                      ...editPatient,
+                      heartRate: Number(e.target.value),
+                    })
+                  }
+                />
+                <input
+                  value={editPatient.bloodOxygen}
+                  onChange={(e) =>
+                    setEditPatient({
+                      ...editPatient,
+                      bloodOxygen: Number(e.target.value),
+                    })
+                  }
+                />
+
+                <button type="submit">Save</button>
+              </form>
+            )}
           </ul>
         ))}
       </div>
